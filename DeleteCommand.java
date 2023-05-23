7f48b0da1332883e831cb1f36e36223c45a03ced
dimadev2 <90557883+dimadev2@users.noreply.github.com>
2023-05-23 10:19:45 +0700

    Delete DeleteCommand.java

--- DeleteCommand.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-import java.util.Map;
-
-public class DeleteCommand implements ICommand{
-    @Override
-    public void Execute(Map<String, Var> varPool, List<String> context) {
-        varPool.remove(context.get(0));
-    }
-}
